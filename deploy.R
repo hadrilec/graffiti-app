1323c98a8059334051456870e2c893b35292cbb7
Hadrien Leclerc <66560093+hadrilec@users.noreply.github.com>
2022-04-15 11:45:15 +0200

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,9 +1,11 @@
+install.packages("magrittr")
 install.packages("devtools")
 devtools::install_github("pyr-opendatafr/R-Insee-Data")
 devtools::install_version("shinydashboardPlus", version = "0.7.5", repos = "http://cran.us.r-project.org")
 devtools::install_github("hadrilec/graffiti-poc")
 
-install.packages("magrittr")
-
 setwd("graffiti-app")
 shiny::runApp()
+
+rsconnect::deployApp()
+
